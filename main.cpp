92c71e2bc625ec1ed49882ef8efb91662f496df0
Chhiring D. Lama <chhiringedu@gmail.com>
2025-02-12 20:31:25 -0800

    testing

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+//ask user to log in as doctor or patient, use a while loop till user selects the right choice?
+//if(choice == doctor){
+//run doctor function
+//} else if(choice == patient) {
+//run patient function
+//} else {
+//invalid input, try again
+//}
+
+
+/*
+ doctor function
+ please login
+ cin >> username - Dobert
+ cin >> password - Password
+ if username != x && password != x
+ cout << try again
+ else go back to log in option
+ 
+ if username == x && password == x
+ cout << "Welcome, Dr. " + username << end;
+ cout << these are your upcoming appointments, selects appointment :)
+ */
+
+/*
+ patient function
+ please login
+ cin >> username - Dobert
+ cin >> password - Password
+ if username != x && password != x
+ cout << try again
+ else go back to log in option
+ 
+ if username == x && password == x
+ cout << "Welcome, " + username << end;
+ cout << "View, Schedule, Cancel appointmnet" << endl;
+ if (userOption == view) {
+    display appoitnments
+ } else if(userOption == schedule){
+    run schedule function;
+ } else if (userOption == cancel) {
+    run cancel functiionl;
+ } else {
+    cout << invalid input please try agian << endl;
+ }
+ 
+ void Schedule(){
+    hold data in an object/class
+    cout << please choose a doctor: << endl;
+    display doctor list from an array or txt file; use switch case
+    cout << what is your purpose of this visit << endl;
+    display options from an array or txt file 1,2,3,4,5,other use switch case
+    cout << plese select time and date << endl
+ }
+ */
